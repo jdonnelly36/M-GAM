60316e942656bd3a9ac88f5932585ad0be0dd830
HaydenMcT <hayden.mctavish@alumni.ubc.ca>
2023-10-21 23:08:18 -0400

    Rerun MICE comparison, more imputations

--- missingness-experiments/mice-imputations/fico.R
+++ missingness-experiments/mice-imputations/fico.R
@@ -1,8 +1,11 @@
-setwd("C:/Users/Hayde/Documents/work/mice")
+# Before running,
+# set to appropriate working directory
+# (exact setwd command ommitted, because it depends on R setup)
 
 library("mice")
 library("VIM")
 
+num_imputations <- 20
 
 df <- read.csv('fico_full.csv')
 
@@ -14,15 +17,15 @@ df <- replace(df, df < 0, NA)
 #To start, we're doing imputation off of a semi-supervised learning paradigm
 # so we want to impute just for all features except the target PoorRiskPerf
 df_X <- df[, -24]
-imp_X <- mice(df_X, seed = 2000)
-print(imp_X)
-complete(imp_X)
+imp_X <- mice(df_X, m=num_imputations, seed = 2000)
+#print(imp_X)
+#complete(imp_X)
 
 temp_df = complete(imp_X)
 temp_df$PoorRiskPerformance = df$PoorRiskPerformance
 write.csv(temp_df, 'fico_semi_supervised_imputation_1.csv', row.names=FALSE)
 
-for (i in c(2, 3, 4, 5)) {
+for (i in c(2:num_imputations)) {
   temp_df = complete(imp_X, i)
   temp_df$PoorRiskPerformance = df$PoorRiskPerformance
   write.csv(temp_df, paste('fico_semi_supervised_imputation_', i, '.csv', sep=''), row.names=FALSE)
