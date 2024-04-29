562af3f89f4ffaf889272995bcd1128341bf4ee0
Hayden McTavish <ham51@cs.duke.edu>
2024-04-29 15:43:34 -0400

    no imputation experiment for adult dataset

--- missingness-experiments/nature-mice-imputations/mice_slurm_data_no_imp.py
+++ missingness-experiments/nature-mice-imputations/mice_slurm_data_no_imp.py
@@ -1,8 +1,8 @@
 #!/home/users/ham51/.venvs/fastsparsebuild/bin/python
-#SBATCH --job-name=aps # Job name
+#SBATCH --job-name=adult # Job name
 #SBATCH --mail-type=NONE          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ham51@duke.edu     # Where to send mail
-#SBATCH --output=aps_%j.out
+#SBATCH --output=adult_%j.out
 #SBATCH --ntasks=1                 # Run on a single Node
 #SBATCH --cpus-per-task=16          # All nodes have 16+ cores; about 20 have 40+
 #SBATCH --mem=100gb                     # Job memory request
@@ -32,10 +32,10 @@ metric = 'acc'
 
 print(f'{num_quantiles}')
 
-overall_mi_intercept = False
-overall_mi_ixn = False
-specific_mi_intercept = True
-specific_mi_ixn = True
+overall_mi_intercept = True#False
+overall_mi_ixn = True#False
+specific_mi_intercept = False#True
+specific_mi_ixn = False#True
 
 print(f'{overall_mi_intercept}_{overall_mi_ixn}_{specific_mi_intercept}_{specific_mi_ixn}')
 
@@ -209,6 +209,11 @@ for holdout_set in holdouts:
         y_train_no, y_train_ind, y_train_aug, 
         y_test_no, y_test_ind, y_test_aug) = encoder.binarize_and_augment(
             pd.concat([train, val]), test)
+
+        np.savetxt("no_imp_no_missingness.csv", train_no, delimiter=',')
+        np.savetxt("no_imp_indicator.csv", train_ind, delimiter=",")
+        np.savetxt("no_imp_aug.csv", train_aug, delimiter=',')
+
 
         model_no = fastsparsegams.fit(train_no, y_train_no, loss="Exponential", algorithm="CDPSI", lambda_grid=lambda_grid, 
                                     num_lambda=None, num_gamma=None, max_support_size=s_size)
